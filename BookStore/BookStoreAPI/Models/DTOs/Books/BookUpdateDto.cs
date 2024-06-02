250a80e6f7bccf834eb7156b582c1851264ecd65
MamatqulovMurod <mamatqulovmurod383@gmail.com>
2024-06-02 19:05:21 +0500

    INFRA: Init-DTO

---
+++ BookStore/BookStoreAPI/Models/DTOs/Books/BookUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreAPI.Models.DTOs.Books
+{
+    public class BookUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public Guid CategoryId { get; set; }
+        public IList<Guid> AuthorIds { get; set; }
+    }
+}
