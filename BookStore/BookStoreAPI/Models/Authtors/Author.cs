30dd21c2bd03a508116cbd615098ed8e234ceab1
MamatqulovMurod <mamatqulovmurod383@gmail.com>
2024-06-02 18:14:05 +0500

    INFRA; Initialize Models

---
+++ BookStore/BookStoreAPI/Models/Authtors/Author.cs
@@ -0,0 +1,16 @@
+﻿using BookStoreAPI.Models.BookAuthors;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreAPI.Models.Authtors
+{
+    public class Author
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual ICollection<BookAuthor> BookAuthors { get; set; }
+    }
+}
